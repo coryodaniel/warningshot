181f38b638a25c2b9a82e9166fc5265d478a6afe
Cory ODaniel <github@coryodaniel.com>
2008-10-16 13:21:20 -0700

    Templates and some todo updates

---
+++ test/spec/unit/resolvers/permission_resolver_spec.rb
@@ -0,0 +1,40 @@
+require File.join(%w(. lib resolvers permission_resolver))
+a093209209 9 9 9 900 )
+describe WarningShot::PermissionResolver do
+
+  it 'should have tests registered' do
+    WarningShot::PermissionResolver.tests.empty?.should be(false)
+  end
+
+  it 'should have resolutions registered' do
+    WarningShot::PermissionResolver.resolutions.empty?.should be(false)
+  end
+     
+  describe 'with healing enabled' do
+    describe 'with heal instructions' do
+      it 'should' do
+        pending
+      end
+    end # End healing enabled, instructions provided
+    
+    describe 'without heal instructions' do
+      it 'should' do
+        pending
+      end
+    end # End healing enabled, instructions not provided
+  end # End healing enabled
+  
+  describe 'with healing disabled' do
+    describe 'with heal instructions' do
+      it 'should' do
+        pending
+      end
+    end #End healing disabled, instructions provided
+    
+    describe 'without heal instructions' do
+      it 'should' do
+        pending
+      end
+    end # End healing disabled, instructions not provided
+  end # End healing disabled
+end
